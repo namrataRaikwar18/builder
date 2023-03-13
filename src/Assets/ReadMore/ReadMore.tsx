3a667e1139fc641a7d869e8b1f23be9ec0ac170c
Shrutigupta431 <sg9956855@gmail.com>
2023-03-13 12:11:56 +0530

    LastCommit

--- src/Assets/ReadMore/ReadMore.tsx
+++ src/Assets/ReadMore/ReadMore.tsx
@@ -1,18 +1,18 @@
 import React,{useState} from 'react';
-
+import "./Readmore.css";
 function ReadMore() {
   const [readMore,setReadMore]=useState(false);
-  const extraContent=<div>
+  const extraContent=<div className="head-content" >
       <p className="extra-content">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur neque ab 
         porro quasi culpa nulla rerum quis minus voluptatibus sed hic ad quo sint, libero 
         commodi officia aliquam! Maxime.
       </p>
   </div>
-  const linkName=readMore?'Read Less << ':' Lorem ipsum dolor sit amet consectetur.... more '
+  const linkName=readMore?'Read Less<< ':' Lorem ipsum dolor sit .... more '
   return (
-    <div className="App">
-      <a className="read-more-link" onClick={()=>{setReadMore(!readMore)}}><h4>{linkName}</h4></a>
+    <div className="App head-content">
+      <a className="read-more-link" onClick={()=>{setReadMore(!readMore)}}>{linkName}</a>
       {readMore && extraContent}
     </div>
   );
